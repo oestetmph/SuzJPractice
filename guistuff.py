904870615b7c2b0d9aafb3f7d6fb5f2197e55ce5
oestetmph <oestetmph@gmail.com>
2020-07-23 00:37:17 -0500

    added gui stuff to play with

--- guistuff.py
+++ guistuff.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')  # Add a touch of color
+# Add a touch of color
+sg.theme('DarkAmber')
 # All the stuff inside your window.
 layout = [[sg.Text('Some text on Row 1')],
           [sg.Text('Enter something on Row 2'), sg.InputText()],
@@ -11,7 +12,8 @@ window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+    # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel':
         break
     print('You entered ', values[0])
 
